9d46687885858de659719369a9669e74905da56e
Arthur <arthur.jacquemin@sfr.fr>
2018-02-09 12:05:53 +0100

    Delete AppBundle -- Create BalrogBundle

---
+++ src/BalrogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BalrogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BalrogBundle:Default:index.html.twig');
+    }
+}
